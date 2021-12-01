25a9527b02ddb33c5158ab9bfccfa4df4a949688
Junior Ribeiro <57049828+juniorribeiro89@users.noreply.github.com>
2021-11-30 22:22:40 -0300

    Update Banco

--- SegunaNota/Pessoa/src/main/resources/db/migration/V1__CREATE__TABLE__PESSOA.sql
+++ SegunaNota/Pessoa/src/main/resources/db/migration/V1__CREATE__TABLE__PESSOA.sql
@@ -4,9 +4,14 @@ CREATE TABLE TB_PESSOA
     nome VARCHAR,
     email VARCHAR ,
     telefone VARCHAR,
-    curso VARCHAR
 
 );
 
-INSERT INTO TB_PESSOA (nome, email,  telefone, curso) VALUES ('evandro', '1234567', '12345678912',  'internet');
-INSERT INTO TB_PESSOA (nome, email,  telefone,  curso) VALUES ('debora', '7419632', '74185236912',  'enfermagem');
+INSERT INTO TB_PESSOA (nome, email,  telefone)
+VALUES ('Junior', '5555555', '88888888888',  'Mecanica');
+INSERT INTO TB_PESSOA (nome, email,  telefone)
+VALUES ('Leonardo', '111111', '77777777777',  'Engenharia Civil');
+INSERT INTO TB_PESSOA (nome, email,  telefone)
+VALUES ('Jaira', '2222222', '66666666666',  'Veterinaria');
+INSERT INTO TB_PESSOA (nome, email,  telefone)
+VALUES ('Carlos', '444444', '36363636363',  'Marinheiro');
